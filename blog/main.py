b7451b90c389659d400448e99e94939dc375589c
AbhiPatel2105 <jxx@gmail.com>
2023-03-09 15:15:54 +0530

    testing email

--- blog/main.py
+++ blog/main.py
@@ -41,15 +41,3 @@ def show(id, response: Response, db: Session = Depends(get_db)):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'details': f'blog with {id} is not found.'}
     return blog
-
-
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(
-        models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'blog with {id} is not found.')
-    blog.update(request.dict())
-    db.commit()
-    return f'blog with {id} is updated successfully.'
